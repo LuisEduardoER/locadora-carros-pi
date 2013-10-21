89439d38be074c5d88f1caf00f8ff17d3f0cdb5a
rafale23@gmail.com <rafale23@gmail.com@e156be26-4573-6807-7178-6b741d515a9b>
2013-10-21 22:49:09 +0000

    toperson

---
+++ trunk/LocadoraPI4/src/Globais/Geral.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Globais;
+
+/**
+ *
+ * @author rafael.baraldi
+ */
+public class Geral {
+    
+    private static String empresa = "LOCADORA";
+
+    public static String getEmpresa() {
+        return empresa;
+    }
+    public static void setEmpresa(String empresa) {
+        Geral.empresa = empresa;
+    }
+    
+}
