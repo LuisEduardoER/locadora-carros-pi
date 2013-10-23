ac1e8ba974f051a8d1fd74af9d64b0e5cb3c74cb
pesado_iron_maiden <pesado_iron_maiden@hotmail.com>
2013-10-23 17:22:34 +0000

    conexao com banco de dados.
    tela de login funcionando

---
+++ LocadoraPI4/src/Classes/Usuario.java
@@ -0,0 +1,124 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Usuario {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public int getPermissao() {
+        return permissao;
+    }
+
+    public void setPermissao(int permissao) {
+        this.permissao = permissao;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public String getCategoria_carta() {
+        return categoria_carta;
+    }
+
+    public void setCategoria_carta(String categoria_carta) {
+        this.categoria_carta = categoria_carta;
+    }
+
+    public int getId_endereco() {
+        return id_endereco;
+    }
+
+    public void setId_endereco(int id_endereco) {
+        this.id_endereco = id_endereco;
+    }
+
+    public int getId_telefone() {
+        return id_telefone;
+    }
+
+    public void setId_telefone(int id_telefone) {
+        this.id_telefone = id_telefone;
+    }
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpf;
+    private String rg;
+    private Date data_nascimento;
+    private int permissao;
+    private String cnh;
+    private String categoria_carta;
+    private int id_endereco;
+    private int id_telefone;
+    
+    
+}
---
+++ LocadoraPI4/src/Controles/Conexao.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controles;
+
+
+import Classes.Usuario;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author Ana Carolina, Beatriz Henrique, Patricia Tertuliano, Weslly Akatuka
+ */
+public class Conexao {
+
+
+public Conexao () {    }    
+
+
+public static Connection connect = null;
+
+
+    public static void Conectar() {
+        System.out.println("Conectando ao banco...");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/locadora_pi4", "root", "");
+            //connect = DriverManager.getConnection("jdbc:mysql://localhost:3307/agenda", "root", "admin");
+            System.out.println("Conectado! :)");
+            
+//            String query = "INSERT INTO agenda (nome, tel) VALUES ('rafael', '9999')";
+//            Statement statement = connect.createStatement();
+//            statement.executeUpdate(query);
+            
+            
+//            ResultSet resultado;
+//            resultado = statement.executeQuery("SELECT * FROM agenda");
+//            
+//            while ( resultado.next() ) {
+//            
+//                System.out.println(resultado.getString("nome"));
+//            }
+    
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    
+    public static void inserir(String nome, String tel){
+        String query = "INSERT INTO agenda (nome, tel) VALUES ('" + nome + "', '" + tel + "')";
+            Statement statement = null;
+        try {
+            statement = connect.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static Usuario buscar_usuario(String busca){
+        Statement statement = null;
+        try {
+            statement = connect.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            ResultSet resultado = null;
+        try {
+            resultado = statement.executeQuery("SELECT * FROM usuario WHERE (email = '" + busca + "')");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        Usuario user = new Usuario();
+        try {
+//            user.setId(Integer.parseInt(resultado.getString("id")));
+            if(!resultado.next()){
+                return null;
+            }
+            else{
+                user.setId(Integer.parseInt(resultado.getString("id")));
+                user.setNome(resultado.getString("nome"));
+                user.setEmail(resultado.getString("email"));
+                user.setSenha(resultado.getString("senha"));
+                user.setCpf(resultado.getString("cpf"));
+                user.setRg(resultado.getString("rg"));
+                //user.setData_nascimento(Date.parse(resultado.getString("data_nascimento")));
+                user.setPermissao(Integer.parseInt(resultado.getString("permissao_id")));
+                user.setCnh(resultado.getString("cnh"));
+                user.setCategoria_carta(resultado.getString("categoria_carta"));
+                user.setId_endereco(Integer.parseInt(resultado.getString("id_endereco")));
+                user.setId_telefone(Integer.parseInt(resultado.getString("id_telefone")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return user;
+        
+    }
+}
